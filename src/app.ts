132c3e9ba2d97b629a547845f0ff451c4977a0f9
MR. Sovon <sovonsdp@gmail.com>
2024-05-21 09:56:22 -0500

    added the necessary resouces

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+// app.use("/api/v1/students", StudentRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
